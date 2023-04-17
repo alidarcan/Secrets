04edea471289ec45455ed6f321adad4ffc9a04e9
Ali Rıza Darcan <99339675+alidarcan@users.noreply.github.com>
2023-04-17 05:03:18 +0300

    md5 crypting

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 //jshint esversion:6
-require('dotenv').config()
+require("dotenv").config();
+var md5 = require("md5");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 
 const app = express();
-
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -19,12 +18,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/secretsDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  secrets: [{secret: String}]
+  secrets: [{ secret: String }],
 });
-
-console.log(process.env.SECRET);
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = new mongoose.model("user", userSchema);
 
@@ -35,22 +30,22 @@ app.get("/", function (req, res) {
 app
   .route("/register")
   .get(function (req, res) {
-      res.render("register");
-    })
+    res.render("register");
+  })
   .post(function (req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: req.body.password,
-    });
     User.findOne({ email: req.body.username }).then(function (userFound) {
-        if (userFound) {
+      if (userFound) {
         console.log("User " + userFound.email + " exists in the database.");
       } else {
+        const newUser = new User({
+          email: req.body.username,
+          password: md5(req.body.password),
+        });
         newUser
           .save()
           .then(function () {
-              console.log("New user saved to the database.");
-              res.render("secrets");
+            console.log("New user saved to the database.");
+            res.render("secrets");
           })
           .catch(function (err) {
             console.log(err);
@@ -66,7 +61,7 @@ app
   })
   .post(function (req, res) {
     const email = req.body.username;
-    const password= req.body.password;
+    const password = md5(req.body.password);
     User.findOne({
       email: email,
     })
@@ -75,21 +70,22 @@ app
           console.log("User found, logging in...");
           res.render("secrets");
         } else {
-            console.log("Check your username and password, try again!");
+          console.log("Check your username and password, try again!");
         }
-    })
-    .catch(function (err) {
+      })
+      .catch(function (err) {
         console.log(err);
-    });
-});
+      });
+  });
 
-app.route("/submit")
-.get(function(req, res){
-    res.render("submit")
-})
-.post(function(req, res){
-    User.updateOne
-})
+app
+  .route("/submit")
+  .get(function (req, res) {
+    res.render("submit");
+  })
+  .post(function (req, res) {
+    User.updateOne;
+  });
 
 app.listen(3000, function (req, res) {
   console.log("Server is running on port 3000.");
